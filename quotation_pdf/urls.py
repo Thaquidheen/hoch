3932ae650faeba1bd623419ef7fcbdff3607b93b
Thaquidheen <thaquidheen@gmail.com>
2025-08-31 05:49:01 +0530

    asdav

--- quotation_pdf/urls.py
+++ quotation_pdf/urls.py
@@ -14,18 +14,18 @@ app_name = 'quotation_pdf'
 
 urlpatterns = [
     # PDF Generation
-    path('generate/<uuid:project_id>/', GenerateQuotationPDFView.as_view(), name='generate_pdf'),
+    path('generate/<int:project_id>/', GenerateQuotationPDFView.as_view(), name='generate_pdf'),
     
     # Preview & Validation
-    # path('preview/<uuid:project_id>/', PreviewQuotationDataView.as_view(), name='preview_data'),
-    path('validate/<uuid:project_id>/', ValidateCustomizationView.as_view(), name='validate_customization'),
-    
+    # path('preview/<int:project_id>/', PreviewQuotationDataView.as_view(), name='preview_data'),
+    path('validate/<int:project_id>/', ValidateCustomizationView.as_view(), name='validate_customization'),
+
     # Customization Management
-    path('customization/<uuid:project_id>/', GetPDFCustomizationView.as_view(), name='get_customization'),
-    path('customization/<uuid:project_id>/save/', SavePDFCustomizationView.as_view(), name='save_customization'),
-    
+    path('customization/<int:project_id>/', GetPDFCustomizationView.as_view(), name='get_customization'),
+    path('customization/<int:project_id>/save/', SavePDFCustomizationView.as_view(), name='save_customization'),
+
     # History & Templates
-    path('history/<uuid:project_id>/', QuotationPDFHistoryView.as_view(), name='pdf_history'),
+    path('history/<int:project_id>/', QuotationPDFHistoryView.as_view(), name='pdf_history'),
     path('templates/', PDFTemplatesListView.as_view(), name='pdf_templates'),
 ]
 
